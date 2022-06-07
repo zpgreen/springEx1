8e692bdb1c8ea733da9eca37d76bd9942ca3bacf
zpgreen28 <zpgreen28@gmail.com>
2022-06-07 16:29:13 +0900

    mustache add

---
+++ src/main/java/com/spring/ex01/controller/FistController.java
@@ -0,0 +1,22 @@
+package com.spring.ex01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class FistController {
+	@GetMapping("/hi")
+	public String greeting(Model model){
+		model.addAttribute("username", "철철");
+		return "greetings";
+	}
+	@GetMapping("/bye")
+	public String byeAction(Model model) {
+		model.addAttribute("nickname", "inchul");
+		return "bye";
+	}
+	
+}
